ac9bd444bf658f51e16dcedcf9d28504e9bb4ca7
robertpaukovic3 <100696918+robertpaukovic3@users.noreply.github.com>
2022-03-10 11:55:02 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+function f1() {   
+    alert("Način1!");
+}    
+
+
+
+let f2= function() {   
+    alert("Način2!");
+}   
+
+const f3=function(){    
+    alert("Način 3"); 
+   
+
+}
+
+function zbroji(a,b){   
+    z=a+b;   
+    return z;
+}  
+
+console.log(zbroji(5,4));     
+
+
+let z2=function(a,b){  
+    return a+b;
+}    
+
+console.log(z2(5,6));
